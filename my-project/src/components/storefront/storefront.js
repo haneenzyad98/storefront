d7e1f14adcceed43b5304ed3108857d382f05e48
Haneen zyad odebat <haneenzyad98@gmail.com>
2021-07-05 22:40:31 +0300

    lab

---
+++ my-project/src/components/storefront/storefront.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Products from './products';
+
+export default function Storefront() {
+
+  return (
+    <>
+      
+      {/* <CurrentCategory /> */}
+      <Products />
+    </>
+  )
+}
